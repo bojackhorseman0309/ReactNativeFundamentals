644fe833bfe394ad7d9fff5f8a30f357293255b7
Alonso Araya Calvo <alonso@elumen.info>
2021-01-13 09:32:39 -0600

    Module 3 Layout Finished

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,29 +1,7 @@
 import React from 'react';
-import {
-  Button,
-  SafeAreaView,
-  Alert,
-  StyleSheet,
-  Platform,
-  StatusBar,
-} from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Button color='orange' title='Click Me'></Button>
-    </SafeAreaView>
-  );
+  return <ViewImageScreen></ViewImageScreen>;
 }
-
-const containerStyle = { backgroundColor: 'orange' };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-  },
-});
